70ac36e5cede00bf071a14f62ddfbf08017adf7a
mak <aravind.murugaiyan@gmail.com>
2017-05-31 20:01:21 +0530

    expression intact

--- src/com/mak/sort/bubblesort/BubbleSort.scala
+++ src/com/mak/sort/bubblesort/BubbleSort.scala
@@ -2,7 +2,7 @@ package com.mak.sort.bubblesort
 
 object BubbleSort {
     def sort(a:Array[Double]) : Unit = {
-    	for (i <- 0 until a.length -1){
+    	for (i <- 0 until a.length-1){
     		for (j <- 0 until a.length-1-i){
     			if (a(j) > a(j+1)){
     				val tmp = a(j)
