3996615cf8815aab06f9ab4927610b3a54dfb2ac
mak <aravind.murugaiyan@gmail.com>
2017-05-31 19:57:07 +0530

    expression made more readable

--- src/com/mak/sort/bubblesort/App.scala
+++ src/com/mak/sort/bubblesort/App.scala
@@ -3,10 +3,10 @@ package com.mak.sort.bubblesort
 object App {
   def main(args: Array[String]): Unit = {
     val arr = Array.fill(10)(math.random)
-    println("Before Sorting:");
+    println("Before Sorting:\n")
     arr.foreach { x => println(x) }
     BubbleSort.sort(arr)
-    println("After Sorting:");
+    println("\nAfter Sorting:\n")
     arr.foreach { x => println(x) }
   }
 }
--- src/com/mak/sort/bubblesort/BubbleSort.scala
+++ src/com/mak/sort/bubblesort/BubbleSort.scala
@@ -1,10 +1,9 @@
 package com.mak.sort.bubblesort
 
 object BubbleSort {
-  
     def sort(a:Array[Double]) : Unit = {
     	for (i <- 0 until a.length -1){
-    		for (j <- 0 until a.length -1 -i){
+    		for (j <- 0 until a.length-1-i){
     			if (a(j) > a(j+1)){
     				val tmp = a(j)
     				a(j) = a(j+1)
